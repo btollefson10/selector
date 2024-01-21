590a55347dfe4570688899ae89612c623e765f75
btollefson10 <36615060+btollefson10@users.noreply.github.com>
2024-01-21 07:38:53 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,137 @@
+// Global variables
+const createdItems = new Map();
+const selectedImagesMap = new Map();
+
+function pushSelectedImagesMap(key){
+    selectedImagesMap.set(key, imagesMap.get(key));
+    if(selectedImagesMap.size >= 4){
+        document.querySelector('#imageGrid').classList.add('selector-full');
+    }
+}
+
+function removeFromSelectedImagesMap(key) {
+    selectedImagesMap.delete(key);
+    if(selectedImagesMap.size<4){
+        document.querySelector('#imageGrid').classList.remove('selector-full');
+    }
+}
+
+function pushCreatedItems(key, item){
+    createdItems.set(key, item);
+    //update the sidebar to add the scratchoff images
+}
+
+function modifyCreatedItem(key){
+    //load them as selected items and remove from the created Items map until it is added again
+}
+
+
 document.addEventListener('DOMContentLoaded', function() {
-    const imageGrid = document.getElementById('imageGrid');
+    /*const imageGrid = document.getElementById('imageGrid');
 
     // Initialize the grid with images
     images.forEach((src, index) => {
         const col = document.createElement('div');
-        col.className = 'col-md-4';
+        col.className = 'col-md-4 p-2';
         col.innerHTML = `
-            <div class="image-wrapper">
+            <div class="image-wrapper" onclick="toggleImageSelection(this)">
                 <img src="${src}" class="img-fluid" alt="Image ${index + 1}">
                 <div class="check-overlay">   
- <img src="https://s3.us-east-2.amazonaws.com/myownfairytale.com/images/check-circle-fill.svg" alt="Headline Image" class="headline-image">
+ <img src="./images/check-circle-fill.svg" alt="Headline Image" class="headline-image">
 
 </div>
             </div>
         `;
         imageGrid.appendChild(col);
+    });*/
+
+    const imageGrid = document.getElementById('imageGrid');
+
+    // Iterate over the imagesMap
+    imagesMap.forEach((src, key) => {
+        const col = document.createElement('div');
+        col.className = 'col-md-3 p-2';
+        col.innerHTML = `
+            <div class="image-wrapper" onclick="toggleImageSelection(this)" data-key="${key}">
+                <img src="${src}" class="img-fluid" alt="Image ${key}">
+                <div class="check-overlay">   
+                    <img src="./images/check-circle-fill.svg" alt="Headline Image" class="headline-image">
+                </div>
+            </div>
+        `;
+        imageGrid.appendChild(col);
     });
 
+
     // Event listener for the lottery button
-    document.getElementById('lotteryButton').addEventListener('click', () => {
+    /*document.getElementById('lotteryButton').addEventListener('click', () => {
         // Clear previous selections
         const wrappers = document.querySelectorAll('.image-wrapper');
         wrappers.forEach(wrapper => wrapper.classList.remove('selected'));
 
         // Get and display 4 random images
-        let selectedImages = getRandomImages(images, 4);
+        selectedImages = getRandomImages(images, 4);
         displayImages(selectedImages);
 
         // Highlight selected images in the grid
-        highlightSelectedImages(selectedImages);
+        highlightSelectedImages();
+    });*/
+
+    document.getElementById('randomImagesButton').addEventListener('click', selectRandomImages);
+
+
+    // Add to Cart button event listener
+    document.getElementById('scratchOff').addEventListener('click', function() {
+        if(selectedImagesMap.size < 4){
+            alert('make sure 4 images are added before adding to cart');
+            return;
+        }
+        // Create a card with selected images
+        let card = createImageCard(createdItems.size + 1);
+
+        // Add the card to the sidebar
+        let sidebar = document.getElementById('sidebar');
+        sidebar.appendChild(card);
+
+        // Store the selected images in the createdItems map
+        pushCreatedItems(createdItems.size + 1, {images: selectedImagesMap.keys()})
+
+        // Reset the random images selection
+        resetSelectedImages();
+
+        // Show the sidebar if it's not already visible
+        sidebar.classList.add('show');
+        sidebar.classList.add('p-3');
     });
 });
 
 // Function to display 3 random images
 function displayImages(imagePaths) {
     const container = document.getElementById('imageContainer');
-    container.innerHTML = imagePaths.map(path => `<img src="${path}" alt="Lottery Image">`).join('');
+    container.innerHTML = imagePaths.map(path => `
+        <div class="col-md-3 p-2">
+            <img src="${path}" alt="Lottery Image">
+        </div>
+    `).join('');
+}
+
+function newDisplayImages(){
+    let index = 0;
+    selectedImagesMap.forEach((value, key) => {
+        index++;
+        const slot = document.getElementById(`slot-${index}`);
+        slot.innerHTML = `<img src="${value}" alt="${key} Image">`;
+    });
+    for (let slotIndex = index; slotIndex < 4; slotIndex++) {
+        console.log(slotIndex);
+        const slot = document.getElementById(`slot-${slotIndex+1}`);
+        slot.innerHTML = `<img src="/images/card-image.svg" alt="placeholder image">`;
+        
+    }
 }
 
 // Function to highlight selected images in the grid
-function highlightSelectedImages(selectedImages) {
+function highlightSelectedImages() {
     // Get the root URL
     let rootUrl = window.location.origin;
 
@@ -56,7 +148,6 @@ function highlightSelectedImages(selectedImages) {
 
 // ... (rest of the functions like getRandomImages, getRandomIndexes remain unchanged)
 
-
 const images = [
     'https://s3.us-east-2.amazonaws.com/myownfairytale.com/images/Cover.png',
     'https://s3.us-east-2.amazonaws.com/myownfairytale.com/images/End.png',
@@ -71,14 +162,32 @@ const images = [
     // ... add paths for all 10 images
 ];
 
-function selectRandomImages() {
-    const wrappers = document.querySelectorAll('.image-wrapper');
-    wrappers.forEach(wrapper => wrapper.classList.remove('selected'));
-    let selectedIndexes = getRandomIndexes(wrappers.length, 4); // Select 4 random images
+const imagesMap = new Map();
 
-    selectedIndexes.forEach(index => {
-        wrappers[index].classList.add('selected');
-    });
+imagesMap.set('1', '/images/1.png');
+imagesMap.set('2', '/images/2.png');
+imagesMap.set('3', '/images/3.png');
+imagesMap.set('4', '/images/4.png');
+imagesMap.set('5', '/images/5.png');
+imagesMap.set('6', '/images/6.png');
+imagesMap.set('7', '/images/7.png');
+imagesMap.set('8', '/images/8.png');
+imagesMap.set('9', '/images/9.png');
+imagesMap.set('10', '/images/10.png');
+
+function selectRandomImages() {
+    let remainingSlots = 4 - selectedImagesMap.size;
+    if (remainingSlots > 0) {
+        let additionalImages = getRandomItems(remainingSlots);
+        additionalImages.forEach((value, key) => {
+            if (!selectedImagesMap.has(key)) {
+                pushSelectedImages(value);
+                pushSelectedImagesMap(key);
+            }
+        });
+        highlightSelectedImages();
+        displayImages(selectedImages);
+    }
 }
 
 function getRandomIndexes(max, count) {
@@ -92,11 +201,6 @@ function getRandomIndexes(max, count) {
     return indexes;
 }
 
-document.getElementById('lotteryButton').addEventListener('click', () => {
-    let selectedImages = getRandomImages(images, 3);
-    displayImages(selectedImages);
-});
-
 function getRandomImages(arr, num) {
     let copy = arr.slice();
     let result = [];
@@ -108,4 +212,94 @@ function getRandomImages(arr, num) {
     }
 
     return result;
+}
+
+function getRandomItems(numItems) {
+    // Filter out preselected items
+    const availableItems = Array.from(imagesMap).filter(([key, value]) => !selectedImagesMap.has(key));
+
+    // Handle case where numItems is greater than available items
+    const itemsToSelect = Math.min(numItems, availableItems.length);
+    
+    const selectedItems = new Map();
+
+    while (selectedItems.size < itemsToSelect) {
+        const randomIndex = Math.floor(Math.random() * availableItems.length);
+        const [key, value] = availableItems[randomIndex];
+
+        // Add to selectedItems map if not already selected
+        if (!selectedItems.has(key)) {
+            selectedItems.set(key, value);
+        }
+    }
+
+    return selectedItems;
+}
+
+function createImageCard(cardNumber) {
+    // Create a card element and add the selected images to it
+    let card = document.createElement('div');
+    card.className = 'card';
+    card.dataset.cardKey = cardNumber;
+    card.addEventListener('click', createdImageCardClick)
+
+    let keys = Array.from(selectedImagesMap.keys());
+    selectRandomImages.forEach( value, key => {
+
+    })
+    keys.forEach(key => {
+        src = 
+        let img = document.createElement('img');
+        img.src = src;
+        img.className = 'card-img-top'; // Bootstrap class
+        card.appendChild(img);
+    });
+
+    return card;
+}
+
+createdImageCardClick = (event) => {
+    target = event.target;
+    cardKey = target.dataset.cardKey;
+    cardItems = createdItems.get(cardKey);
+    selectedImagesMap
+}
+
+function resetSelectedImages() {
+    selectedImagesMap.clear();
+    // Select all elements with the class 'selected'
+    let elements = document.querySelectorAll('.selected');
+
+    // Iterate over the NodeList and remove the class 'selected' from each element
+    elements.forEach(element => {
+        element.classList.remove('selected');
+    });
+
+    newDisplayImages();
+}
+
+function toggleImageSelection(wrapper) {
+    let key = wrapper.dataset.key;
+    if(selectedImagesMap.has(key)){
+        wrapper.classList.remove('selected');
+        removeFromSelectedImagesMap(key);
+    }else if(selectedImagesMap.size < 4){
+        wrapper.classList.add('selected');
+        pushSelectedImagesMap(key);
+    }
+    // Find the image inside the wrapper
+    /*let childImg = wrapper.querySelector('img');
+    if (!childImg) return; // Exit if no image found
+
+    let imgSrc = childImg.getAttribute('src');
+    if (selectedImages.includes(imgSrc)) {
+        wrapper.classList.remove('selected');
+        selectedImages = selectedImages.filter(src => src !== imgSrc);
+    } else if (selectedImages.length < 4) {
+        selectedImages.push(imgSrc);
+        wrapper.classList.add('selected');
+    }*/
+
+    //displayImages(selectedImages);
+    newDisplayImages();
 }
